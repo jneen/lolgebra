71c4a0af3d07be1cfe7d6630fb613eade0578e25
Han Wei <laughinghan@gmail.com>
2010-05-27 02:20:40 -0400

    great documentation Faye! [sic]

--- messages.rb
+++ messages.rb
@@ -6,10 +6,11 @@ get '/chat/:room_name/messages' do
   first = params[:start].to_i
   last = (params[:end] || -1).to_i
 
-  # I'm surprised subscribing multiple times doesn't cause duplicate
-  # messages, you'd think the block would get called as many times as
-  # subscribed. I mean what if I decided at two different times to
-  # subscribe two different blocks to the same channel?
+  # Note that the following doesn't cause duplicates because if a client
+  # has already subscribed to a channel, resubscribing just overwrites
+  # the block originally used to subscribe.
+  # I found this out by looking through the source code.
+  # Documentation fail, but Ruby win!
   env['faye.client'].subscribe "/#{params[:room_name]}" do |message|
     room.messages << Message.new(
       :content => message["message"],
