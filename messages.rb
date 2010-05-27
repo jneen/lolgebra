c08e4e406087c87137191145749c626278a26fe0
Han Wei <laughinghan@gmail.com>
2010-05-26 23:32:21 -0400

    server subscribe and logging works now

--- messages.rb
+++ messages.rb
@@ -6,6 +6,17 @@ get '/chat/:room_name/messages' do
   first = params[:start].to_i
   last = (params[:end] || -1).to_i
 
+  # I'm surprised subscribing multiple times doesn't cause duplicate
+  # messages, you'd think the block would get called as many times as
+  # subscribed. I mean what if I decided at two different times to
+  # subscribe two different blocks to the same channel?
+  env['faye.client'].subscribe "/#{params[:room_name]}" do |message|
+    room.messages << Message.new(
+      :content => message["message"],
+      :name => message["name"]
+    )
+  end
+
   return {
     :status => 1,
     :messages => room.messages[first..last].map(&:to_hash),
