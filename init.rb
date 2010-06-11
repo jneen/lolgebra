e346aebb47d67c226a08dfd1ae8b679a62ac2f55
Han Wei <laughinghan@gmail.com>
2010-06-10 23:41:24 -0400

    loading a room, not getting messages from a room, should trigger subscribe

--- init.rb
+++ init.rb
@@ -18,7 +18,22 @@ get '/health' do
 end
 
 get '/chat/:room' do
-  @room = Room[params[:room].downcase]
+  room_name = params[:room].downcase
+  @room = Room[room_name]
+
+  # Note that if a client has already subscribed to a channel,
+  # resubscribing just overwrites the block originally used to
+  # subscribe. (I had originally assumed there'd be multiple
+  # subscriptions leading to duplicate messages.)
+  # I found this out by looking through the source code.
+  # Documentation fail, but Ruby win!
+  env['faye.client'].subscribe "/#{room_name}" do |message|
+    @room.messages << Message.new(
+      :content => message["message"],
+      :name => message["name"]
+    )
+  end
+
   @username = params[:name] || "Anonymous"
   erb :chat
 end
--- messages.rb
+++ messages.rb
@@ -6,19 +6,6 @@ get '/chat/:room_name/messages' do
   first = params[:start].to_i
   last = (params[:end] || -1).to_i
 
-  # Note that if a client has already subscribed to a channel,
-  # resubscribing just overwrites the block originally used to
-  # subscribe. (I had originally assumed there'd be multiple
-  # subscriptions leading to duplicate messages.)
-  # I found this out by looking through the source code.
-  # Documentation fail, but Ruby win!
-  env['faye.client'].subscribe "/#{params[:room_name]}" do |message|
-    room.messages << Message.new(
-      :content => message["message"],
-      :name => message["name"]
-    )
-  end
-
   return {
     :status => 1,
     :messages => room.messages[first..last].map(&:to_hash),
